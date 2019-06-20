0cd93f50c828f8384d2cb36d4f89fff9cb837b55
chenxz <563859095@qq.com>
2019-06-20 14:54:43 +0800

    commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 
-class App extends StatefulWidget{
+class App extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return null;
+    return new AppState();
+  }
+}
+
+class AppState extends State<App> {
+  String textToShow = "I Like Flutter";
+
+  void _updateText() {
+    setState(() {
+      // update the text
+      textToShow = "Flutter is Awesome!";
+    });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(""),
+      ),
+      body: new Center(child: new Text(textToShow)),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _updateText,
+        tooltip: 'Update Text',
+        child: new Icon(Icons.update),
+      ),
+    );
+  }
 }
