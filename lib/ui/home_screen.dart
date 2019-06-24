8168eaac9e832c45fd5d770e13b3697d8783d444
chenxz <563859095@qq.com>
2019-06-24 17:17:06 +0800

    commit

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,31 +1,228 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_wanandroid/common/common.dart';
+import 'package:flutter_wanandroid/data/api/apis_service.dart';
+import 'package:flutter_wanandroid/data/model/article_model.dart';
+import 'package:flutter_wanandroid/ui/base_widget.dart';
+import 'package:flutter_wanandroid/ui/home_banner_screen.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends BaseWidget {
   @override
-  State<StatefulWidget> createState() {
-    return new HomeScreenState();
+  BaseWidgetState<BaseWidget> attachState() {
+    return HomeScreenState();
   }
 }
 
-class HomeScreenState extends State<HomeScreen> {
-  String textToShow = "I Like Flutter";
+class HomeScreenState extends BaseWidgetState<HomeScreen> {
+  /// 首页文章列表数据
+  List<Article> _articles = new List();
 
-  void _updateText() {
-    setState(() {
-      // update the text
-      textToShow = "Flutter is Awesome!";
+  /// listview 控制器
+  ScrollController _scrollController = new ScrollController();
+
+  /// 是否显示悬浮按钮
+  bool _isShowFAB = false;
+
+  /// 页码，从0开始
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    setAppBarVisible(false);
+
+    showLoading();
+    getArticleList();
+
+    _scrollController.addListener(() {
+      /// 滑动到底部，加载更多
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        getMoreArticleList();
+      }
+      if (_scrollController.offset < 200 && _isShowFAB) {
+        setState(() {
+          _isShowFAB = false;
+        });
+      } else if (_scrollController.offset >= 200 && !_isShowFAB) {
+        setState(() {
+          _isShowFAB = true;
+        });
+      }
     });
   }
 
+  Future<Null> getArticleList() async {
+    _page = 0;
+    ApiService().getArticleList((ArticleModel articleModel) {
+      if (articleModel.errorCode == Constants.STATUS_SUCCESS) {
+        if (articleModel.data.datas.length > 0) {
+          showContent();
+          setState(() {
+            _articles.clear();
+            _articles.addAll(articleModel.data.datas);
+          });
+        } else {
+          showEmpty();
+        }
+      } else {
+        Fluttertoast.showToast(msg: articleModel.errorMsg);
+      }
+    }, (DioError error) {
+      /// 发生错误
+      print(error.response);
+      setState(() {
+        showError();
+      });
+    }, _page);
+  }
+
+  Future<Null> getMoreArticleList() async {
+    _page++;
+    ApiService().getArticleList((ArticleModel articleModel) {
+      if (articleModel.errorCode == Constants.STATUS_SUCCESS) {
+        if (articleModel.data.datas.length > 0) {
+          showContent();
+          setState(() {
+            _articles.addAll(articleModel.data.datas);
+          });
+        } else {
+          Fluttertoast.showToast(msg: "没有更多数据了");
+        }
+      }
+    }, (DioError error) {
+      /// 发生错误
+      print(error.response);
+      setState(() {
+        showError();
+      });
+    }, _page);
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Center(child: new Text(textToShow)),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _updateText,
-        tooltip: 'Update Text',
-        child: new Icon(Icons.update),
-      ),
+  AppBar attachAppBar() {
+    return AppBar(
+      title: Text(""),
     );
+  }
+
+  @override
+  Widget attachContentWidget(BuildContext context) {
+    return Scaffold(
+      body: RefreshIndicator(
+        displacement: 15,
+        onRefresh: getArticleList,
+        child: ListView.separated(
+            itemBuilder: itemView,
+            separatorBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 0.5,
+                color: Colors.black26,
+              );
+            },
+            physics: new AlwaysScrollableScrollPhysics(),
+            controller: _scrollController,
+            // 包含轮播和加载更多
+            itemCount: _articles.length + 2),
+      ),
+      floatingActionButton: !_isShowFAB
+          ? null
+          : FloatingActionButton(
+              child: Icon(Icons.arrow_upward),
+              onPressed: () {
+                /// 回到顶部时要执行的动画
+                _scrollController.animateTo(0,
+                    duration: Duration(milliseconds: 2000), curve: Curves.ease);
+              },
+            ),
+    );
+  }
+
+  @override
+  void onClickErrorWidget() {
+    showLoading();
+    getArticleList();
+  }
+
+  Widget itemView(BuildContext context, int index) {
+    if (index == 0) {
+      return Container(
+        height: 200,
+        color: Colors.transparent,
+        child: new HomeBannerScreen(),
+      );
+    }
+
+    if (index < _articles.length - 1) {
+      return InkWell(
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    _articles[index - 1].author,
+                    style: TextStyle(fontSize: 12),
+                    textAlign: TextAlign.left,
+                  ),
+                  Expanded(
+                    child: Text(
+                      _articles[index - 1].niceDate,
+                      style: TextStyle(fontSize: 12),
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _articles[index - 1].title,
+                      maxLines: 2,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: const Color(0xFF3D4E5F),
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.fromLTRB(16, 8, 16, 16),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      _articles[index - 1].superChapterName,
+                      style: TextStyle(fontSize: 12),
+                      textAlign: TextAlign.left,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+    return null;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 }
