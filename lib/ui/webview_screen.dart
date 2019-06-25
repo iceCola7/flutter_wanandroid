f9b304be7c5183cfeff8cdd0a3948e48970bde00
chenxz <563859095@qq.com>
2019-06-25 11:12:55 +0800

    commit

---
+++ lib/ui/webview_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class WebViewScreen extends StatefulWidget {
+  /// 标题
+  String title;
+
+  /// 链接
+  String url;
+
+  WebViewScreen({Key key, @required this.title, @required this.url})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return new WebViewScreenState();
+  }
+}
+
+class WebViewScreenState extends State<WebViewScreen> {
+  bool isLoad = true;
+
+  final flutterWebViewPlugin = new FlutterWebviewPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    flutterWebViewPlugin.onStateChanged.listen((state) {
+      debugPrint('state:_' + state.type.toString());
+      if (state.type == WebViewState.finishLoad) {
+        setState(() {
+          isLoad = false;
+        });
+      } else if (state.type == WebViewState.startLoad) {
+        setState(() {
+          isLoad = true;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new WebviewScaffold(
+      url: widget.url,
+      appBar: new AppBar(
+        elevation: 0.4,
+        title: new Text(widget.title),
+        bottom: new PreferredSize(
+            child: isLoad
+                ? new LinearProgressIndicator()
+                : new Divider(
+                    height: 1.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+            preferredSize: Size.fromHeight(1.0)),
+      ),
+      withZoom: false,
+      withLocalStorage: true,
+      withJavascript: true,
+    );
+  }
+}
