355f24fe1696fb118d418e2245e453a0b3a2dc36
chenxz <563859095@qq.com>
2019-07-25 09:42:11 +0800

    commit

--- lib/ui/wechat_screen.dart
+++ lib/ui/wechat_screen.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_wanandroid/common/common.dart';
+import 'package:flutter_wanandroid/common/user.dart';
 import 'package:flutter_wanandroid/data/api/apis_service.dart';
+import 'package:flutter_wanandroid/data/model/base_model.dart';
 import 'package:flutter_wanandroid/data/model/wx_article_model.dart';
 import 'package:flutter_wanandroid/data/model/wx_chapters_model.dart';
 import 'package:flutter_wanandroid/ui/base_widget.dart';
@@ -249,6 +251,21 @@ class WXArticleScreenState extends State<WXArticleScreen> {
                       style: TextStyle(fontSize: 12),
                       textAlign: TextAlign.left,
                     ),
+                  ),
+                  InkWell(
+                    child: Container(
+                      child: Image(
+                        // color: Colors.black12,
+                        image: AssetImage(item.collect
+                            ? 'images/ic_like.png'
+                            : 'images/ic_like_not.png'),
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                    onTap: () {
+                      addOrCancelCollect(item);
+                    },
                   )
                 ],
               ),
@@ -258,6 +275,42 @@ class WXArticleScreenState extends State<WXArticleScreen> {
       );
     }
     return null;
+  }
+
+  /// 添加收藏或者取消收藏
+  void addOrCancelCollect(item) {
+    List<String> cookies = User.singleton.cookie;
+    if (cookies == null) {
+      Fluttertoast.showToast(msg: '请先登录~');
+    } else {
+      if (item.collect) {
+        ApiService().cancelCollection((BaseModel model) {
+          if (model.errorCode == Constants.STATUS_SUCCESS) {
+            Fluttertoast.showToast(msg: '已取消收藏~');
+            setState(() {
+              item.collect = false;
+            });
+          } else {
+            Fluttertoast.showToast(msg: '取消收藏失败~');
+          }
+        }, (DioError error) {
+          print(error.response);
+        }, item.id);
+      } else {
+        ApiService().addCollection((BaseModel model) {
+          if (model.errorCode == Constants.STATUS_SUCCESS) {
+            Fluttertoast.showToast(msg: '收藏成功~');
+            setState(() {
+              item.collect = true;
+            });
+          } else {
+            Fluttertoast.showToast(msg: '收藏失败~');
+          }
+        }, (DioError error) {
+          print(error.response);
+        }, item.id);
+      }
+    }
   }
 
   @override
