0b2c74c303bcbaeacd1b53fdfa1de99f30f236c1
chenxz <563859095@qq.com>
2019-07-28 22:43:21 +0800

    commit

--- lib/ui/todo_screen.dart
+++ lib/ui/todo_screen.dart
@@ -1,4 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_wanandroid/common/common.dart';
+import 'package:flutter_wanandroid/data/api/apis_service.dart';
+import 'package:flutter_wanandroid/data/model/base_model.dart';
 import 'package:flutter_wanandroid/ui/base_widget.dart';
 
 class TodoScreen extends BaseWidget {
@@ -9,6 +13,17 @@ class TodoScreen extends BaseWidget {
 }
 
 class TodoScreenState extends BaseWidgetState<TodoScreen> {
+  /// 获取TODO列表数据
+  Future<Null> getTodoList() async {
+    ApiService().getTodoList((BaseModel model) {
+      if (model.errorCode == Constants.STATUS_SUCCESS) {
+      } else {}
+    }, (DioError error) {
+      print(error.response);
+      showError();
+    });
+  }
+
   @override
   AppBar attachAppBar() {
     return new AppBar(
