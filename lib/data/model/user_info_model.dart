a46961fded8bdf8ba6739fc9f6b5457e70c44e7a
chenxz <563859095@qq.com>
2020-06-02 22:06:42 +0800

    fix UserInfoModel

--- lib/data/model/user_info_model.dart
+++ lib/data/model/user_info_model.dart
@@ -25,14 +25,17 @@ class UserInfoModel {
 
 class UserInfoBean {
   int coinCount;
+  int level;
   int rank;
   int userId;
   String username;
 
-  UserInfoBean({this.coinCount, this.rank, this.userId, this.username});
+  UserInfoBean(
+      {this.coinCount, this.level, this.rank, this.userId, this.username});
 
   UserInfoBean.fromJson(Map<String, dynamic> json) {
     coinCount = json['coinCount'];
+    level = json["level"];
     rank = json['rank'];
     userId = json['userId'];
     username = json['username'];
@@ -41,6 +44,7 @@ class UserInfoBean {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['coinCount'] = this.coinCount;
+    data["level"] = this.level;
     data['rank'] = this.rank;
     data['userId'] = this.userId;
     data['username'] = this.username;
