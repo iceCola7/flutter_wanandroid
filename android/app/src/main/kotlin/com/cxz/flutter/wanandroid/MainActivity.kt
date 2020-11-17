12a03f4250b799a7becd7e664c61f041cf27991e
chenxz <563859095@qq.com>
2020-11-17 16:00:11 +0800

    fix

--- android/app/src/main/kotlin/com/cxz/flutter/wanandroid/MainActivity.kt
+++ android/app/src/main/kotlin/com/cxz/flutter/wanandroid/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.cxz.flutter.wanandroid
 
-import android.os.Bundle
-
 import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
 }
